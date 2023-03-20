48e2098297cbfbdac0426d12c30c718f09e718ea
ritik2102 <ritik21feb@gmail.com>
2023-03-20 13:06:28 +0530

    First commit

---
+++ first-app.js
@@ -0,0 +1,12 @@
+// Lecture 5
+// // our code is not using the browser but our internal engine to run code  
+// // console.log("Hello from Node.js");
+
+// // fs is used to import the file system
+// const fs=require("fs");
+
+// // to write something into a file(the first argument is the file name and the second is the data we want to write in the file)
+// // executing this command will create the file in our current directory
+// fs.writeFileSync('hello.txt','Hello from Node.js');
+
+console.log("hello world");
